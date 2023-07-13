77a7a0eb956ed8bf4afcef5cea8a2c47d6040c14
Maxence Lavenu <maxence.lavenu@outlook.fr>
2023-07-14 00:51:19 +0200

    Added MDX support (layout, tailwind plugin)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
-   },
+  },
   site: 'https://maxencelav.github.io',
   base: '/dotpro',
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
