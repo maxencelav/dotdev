85132df3cfcdb80a97fc19d69fef9afd3da3166c
Maxence Lavenu <maxence.lavenu@outlook.fr>
2023-08-06 01:01:48 +0200

    Added proper 404 page and reworked navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		// match 404 page
+		'./src/pages/404.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+	],
 	theme: {
 		extend: {},
 	},
